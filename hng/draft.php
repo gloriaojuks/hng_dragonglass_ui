fa7b79e46c31724be13e57f4b6909982be89b991
gloria ojuks <gloriaojuks@gmail.com>
2019-04-06 20:45:24 +0100

    draft page

--- hng/draft.php
+++ hng/draft.php
@@ -1,42 +1,60 @@
 <!DOCTYPE html>
-	<html>
-		<?php include 'includes/head.php'; ?>
-	<body>
-		<div class="parent">
-			<div class="container padd">
-				<div class="row">
-					<div class="col-sm-3">
-						<?php include'includes/nav.php'; ?>
-					</div>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="style.css" type="text/css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-					<div class="col-sm-9">
-						<div class="row">
-							<div class="col-sm-6">
-								<h4 class="text-left my-journal">MY JOURNAL</h4>
-							</div>
-							<div class="col-sm-6">
-								<h4 class="text-right"><a href="/" class="was-cta">Write a story</a></h4>
-							</div>
-						</div>
-						<br><br>
-						<div>
-							<div class="draft-heading">
-								<!-- replace 10 in sub tag with a php variable for number of published works -->
-								<span class="published-link"><a href="/">Published</a><sub class="no_of_pub font-italic">10</sub></span> <a href="/" class="drafts-link">Drafts</a> <hr>
-							</div>
+<div class="container">
+   
+  <div class="row">
+        <div class="col-sm-6 first_col">
+            <img src="../images/profile.png" alt="">
+            <h4>GLORIA <span style="color:rgb(204, 204, 204)">OJUKWU</span></h4>
+            <p style=>Software Developer at Phree Media,<br>Full Stack Web Developer and <br>Music Lover</p> 
 
-							<div class="drafts-section">
-								<!-- if there are drafts, do some php if...else statement to view drafts -->
-								<!-- No drafts for now as this is a basic implementation -->
-								<div class="drafts">
-									<p class="text-center">You have no drafts</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</body>
-	</html>	
-	
+            <p><img src="https://img.icons8.com/ios/20/f35517/link.png">hng.com/Gloria</p>
+            
+            <p><button type="button" class="btn btn-primary story_btn">Edit Profile</button></p>
+            
+            <p><hr style="width: 130px"></p>
+
+            <ul class="menu">
+                <li>Home</li><br>
+                <li>Drafts</li><br>
+                <li>My Jounal</li><br>
+                <li>Other Journals</li><br><br>
+
+                <li style="color:rgb(204, 204, 204)">SIGN OUT</li>
+            </ul>
+
+
+        </div>
+    
+    <div class="col-sm-6">
+        <div class="story_col">
+        <div class="col-sm-6">
+           <p>MY JOURNAL</p><br>
+           <p>Published <span style="color:rgb(204, 204, 204);font-size:12px">to</span> Drafts</p>
+           <hr style="width:400px"><br><br><br><br><br><br><br>
+           <p style="text-align: center">You have no drafts</p>
+        </div>
+        </div>
+
+        <div class="col-sm-6 second_col">
+            <button type="button" class="btn btn-primary story_btn">Write a story</button>  <br><br>
+        </div>
+
+        </div>
+    </div>
+  </div>
+</div>
+    
+</body>
+</html>
